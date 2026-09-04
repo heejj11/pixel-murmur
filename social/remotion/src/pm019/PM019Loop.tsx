d56a169078ad39f44b95030d4747347b1e654145
jeongheejeong <133382959+heejj11@users.noreply.github.com>
2026-09-04 18:14:40 +0900

    Polish PM-019 scene transitions

--- social/remotion/src/pm019/PM019Loop.tsx
+++ social/remotion/src/pm019/PM019Loop.tsx
@@ -1,7 +1,5 @@
 import { Audio } from "@remotion/media";
-import { TransitionSeries, linearTiming } from "@remotion/transitions";
-import { fade } from "@remotion/transitions/fade";
-import { staticFile } from "remotion";
+import { Series, staticFile } from "remotion";
 import { SceneAction } from "./SceneAction";
 import { SceneClosing } from "./SceneClosing";
 import { SceneMechanism } from "./SceneMechanism";
@@ -9,25 +7,17 @@ import { SceneMechanism } from "./SceneMechanism";
 export const PM019Loop: React.FC = () => {
   return (
     <>
-      <TransitionSeries>
-        <TransitionSeries.Sequence durationInFrames={78} name="01 Action hook">
+      <Series>
+        <Series.Sequence durationInFrames={72} name="01 Action hook">
           <SceneAction />
-        </TransitionSeries.Sequence>
-        <TransitionSeries.Transition
-          presentation={fade()}
-          timing={linearTiming({ durationInFrames: 6 })}
-        />
-        <TransitionSeries.Sequence durationInFrames={54} name="02 Mechanism">
+        </Series.Sequence>
+        <Series.Sequence durationInFrames={48} name="02 Mechanism">
           <SceneMechanism />
-        </TransitionSeries.Sequence>
-        <TransitionSeries.Transition
-          presentation={fade()}
-          timing={linearTiming({ durationInFrames: 6 })}
-        />
-        <TransitionSeries.Sequence durationInFrames={60} name="03 Color vote">
+        </Series.Sequence>
+        <Series.Sequence durationInFrames={60} name="03 Color vote">
           <SceneClosing />
-        </TransitionSeries.Sequence>
-      </TransitionSeries>
+        </Series.Sequence>
+      </Series>
       <Audio
         src={staticFile("pm019/audio/pm019-original-music-loop-6s-v02.wav")}
         volume={0.88}
--- social/remotion/src/pm019/PM019Reel.tsx
+++ social/remotion/src/pm019/PM019Reel.tsx
@@ -1,7 +1,5 @@
 import { Audio } from "@remotion/media";
-import { TransitionSeries, linearTiming } from "@remotion/transitions";
-import { fade } from "@remotion/transitions/fade";
-import { staticFile } from "remotion";
+import { Series, staticFile } from "remotion";
 import { SceneAction } from "./SceneAction";
 import { SceneClosing } from "./SceneClosing";
 import { SceneMechanism } from "./SceneMechanism";
@@ -9,25 +7,17 @@ import { SceneMechanism } from "./SceneMechanism";
 export const PM019Reel: React.FC = () => {
   return (
     <>
-      <TransitionSeries>
-        <TransitionSeries.Sequence durationInFrames={102} name="01 Action hook">
+      <Series>
+        <Series.Sequence durationInFrames={96} name="01 Action hook">
           <SceneAction />
-        </TransitionSeries.Sequence>
-        <TransitionSeries.Transition
-          presentation={fade()}
-          timing={linearTiming({ durationInFrames: 6 })}
-        />
-        <TransitionSeries.Sequence durationInFrames={84} name="02 Mechanism">
+        </Series.Sequence>
+        <Series.Sequence durationInFrames={78} name="02 Mechanism">
           <SceneMechanism />
-        </TransitionSeries.Sequence>
-        <TransitionSeries.Transition
-          presentation={fade()}
-          timing={linearTiming({ durationInFrames: 6 })}
-        />
-        <TransitionSeries.Sequence durationInFrames={96} name="03 Color vote">
+        </Series.Sequence>
+        <Series.Sequence durationInFrames={96} name="03 Color vote">
           <SceneClosing />
-        </TransitionSeries.Sequence>
-      </TransitionSeries>
+        </Series.Sequence>
+      </Series>
       <Audio
         src={staticFile("pm019/audio/pm019-original-music-9s-v02.wav")}
         volume={0.88}
